e7a7c648aa18747296b8dc633cf13eb5c59212ff
Nicholas Rakotomihamina <nico@nrako.com>
2013-11-25 23:22:22 +0100

    loginitems

--- modules/people/manifests/nrako.pp
+++ modules/people/manifests/nrako.pp
@@ -5,8 +5,8 @@ class people::nrako {
   include people::nrako::dotfiles
   include people::nrako::keyboards
   include people::nrako::homebrew
+  include people::nrako::loginitems
   # #include people::nrako::git
   # include people::nrako::symlinks
-  # include people::nrako::loginitems
   # include people::nrako::sublime_text_3
 }
---
+++ modules/people/manifests/nrako/loginitems.pp
@@ -0,0 +1,10 @@
+class people::nrako::loginitems {
+
+  osx_login_item {
+    'Alfred 2':
+      name    => 'Alfred 2',
+      path    => '/Applications/Alfred 2.app',
+      hidden  => true;
+  }
+
+}
