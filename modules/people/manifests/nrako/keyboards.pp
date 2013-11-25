c81181662ce49579b9dd41ce7cdcf3dd69ee3332
Nicholas Rakotomihamina <nico@nrako.com>
2013-11-25 23:46:23 +0100

    set plist for keyboaard

--- modules/people/manifests/nrako/keyboards.pp
+++ modules/people/manifests/nrako/keyboards.pp
@@ -1,9 +1,15 @@
 class people::nrako::keyboards {
-  $plist = "com.apple.HIToolbox.plist"
+  $homedir = "/Users/${::boxen_user}"
 
   file { "/Users/${::boxen_user}/Library/Keyboard Layouts/Swiss FR Dev.keylayout":
     ensure  => 'link',
     source  => 'puppet:///modules/people/Swiss FR Dev.keylayout',
+    require => File["${boxen::config::srcdir}"],
+  }
+
+  file { "${homedir}/Library/Preferences/com.apple.HIToolbox.plist":
+    ensure  => link,
+    source  => 'puppet:///modules/people/com.apple.HIToolbox.plist',
     require => File["${boxen::config::srcdir}"],
   }
 
